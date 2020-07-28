b2877789e7c47a4b7ca4e87edbc495ceb0f6451c
Enes <losthell92@gmail.com>
2020-07-28 11:25:49 +0200

    Added CRUD operations, controllers and .hbs files.

---
+++ scripts/controllers/movie.js
@@ -0,0 +1,84 @@
+import {createMovie, getMoviesByOwner, getMovieById, editMovie, deleteMovie} from '../data.js';
+
+export default async function () {
+    this.partials = {
+        header: await this.load('./templates/common/header.hbs'),
+        footer: await this.load('./templates/common/footer.hbs'),
+    }
+
+    this.partial('./templates/movie/create.hbs', this.app.userData);
+}
+
+export async function create() {
+    if (this.params.title.length < 5) {
+        alert('Sorry title must be minimum 5 characters short!');
+        return;
+    } else if (Number(this.params.tickets) <= 0) {
+        alert('Sorry tickets cannot be below or equal to 0');
+        return;
+    }
+    const newMovie = {
+        title: this.params.title,
+        image: this.params.imageUrl,
+        description: this.params.description,
+        genres: this.params.genres,
+        tickets: Number(this.params.tickets),
+        ownerId: localStorage.getItem('userId')
+    }
+    await createMovie(newMovie);
+    this.redirect('#/cinema');
+}
+
+export async function myMovies() {
+    this.partials = {
+        header: await this.load('./templates/common/header.hbs'),
+        footer: await this.load('./templates/common/footer.hbs'),
+    }
+
+    const ownerId = localStorage.getItem('userId');
+    const movies = await getMoviesByOwner(ownerId);
+
+    const data = Object.assign({movies}, this.app.userData)
+
+    this.partial('./templates/movie/mymovie.hbs', data);
+}
+
+export async function editMyMovie(currentMovie) {
+    this.partials = {
+        header: await this.load('./templates/common/header.hbs'),
+        footer: await this.load('./templates/common/footer.hbs'),
+    }
+
+    const movie = await getMovieById(currentMovie.params.id);
+
+    const data = Object.assign({movie}, this.app.userData)
+
+    this.partial('./templates/movie/edit.hbs', data);
+}
+
+export async function movieToDelete(currentMovie) {
+    this.partials = {
+        header: await this.load('./templates/common/header.hbs'),
+        footer: await this.load('./templates/common/footer.hbs'),
+    }
+
+    const movie = await getMovieById(currentMovie.params.id);
+
+    const data = Object.assign({movie}, this.app.userData)
+
+    this.partial('./templates/movie/delete.hbs', data);
+}
+
+export async function deleteMyMovie(objectId) {
+    this.partials = {
+        header: await this.load('./templates/common/header.hbs'),
+        footer: await this.load('./templates/common/footer.hbs'),
+    }
+    console.log(objectId)
+    const movie = await deleteMovie(objectId);
+
+    console.log(movie)
+    const data = Object.assign({movie}, this.app.userData)
+
+    this.partial('./templates/movie/delete.hbs', data);
+}
